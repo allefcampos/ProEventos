fedd566f7e69d140dac114bb324dd1f5ed70806c
Allef Campos <allef-cleiton88@hotmail.com>
2022-07-13 19:10:58 -0300

    Atualizando mód. eventos com novas funcionalidades

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any;
+  eventos: any = [];
+  eventosFiltrados: any = [];
+  isVisible = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -19,9 +39,8 @@ export class EventosComponent implements OnInit {
   private getEventos() {
     this.http.get("https://localhost:5001/api/Evento").subscribe(
       (dados) => {
-        console.log(dados);
-
         this.eventos = dados;
+        this.eventosFiltrados = this.eventos;
       },
       (error) => console.log(error)
     )
